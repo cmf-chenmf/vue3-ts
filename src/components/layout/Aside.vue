4eec7e7ac0a5a5f8d63789c6b2dc3e8132ea14c1
CMF-chenmengfei <325949568@qq.com>
2022-05-06 13:52:37 +0800

    引入element-plus图标库

--- src/components/layout/Aside.vue
+++ src/components/layout/Aside.vue
@@ -7,6 +7,7 @@ import { mainStore } from '@/store'
 import { storeToRefs } from 'pinia'
 const useStore = mainStore();
 const { collapse } = storeToRefs(useStore);
+console.log(collapse);
 
 
 </script>
@@ -19,23 +20,23 @@ const { collapse } = storeToRefs(useStore);
         <el-sub-menu index="1">
           <template #title>
             <el-icon><location /></el-icon>
-            <span>Navigator One</span>
+            <span>首页</span>
           </template>
           <el-menu-item-group title="Group One">
             <el-menu-item index="1-1">item one</el-menu-item>
             <el-menu-item index="1-2">item one</el-menu-item>
           </el-menu-item-group>
           <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
+            <el-menu-item index="1-3">表单页</el-menu-item>
           </el-menu-item-group>
           <el-sub-menu index="1-4">
             <template #title>item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
+            <el-menu-item index="1-4-1">表格页</el-menu-item>
           </el-sub-menu>
         </el-sub-menu>
         <el-menu-item index="2">
           <el-icon><icon-menu /></el-icon>
-          <span>Navigator Two</span>
+          <span>图表页</span>
         </el-menu-item>
         <el-menu-item index="3" disabled>
           <el-icon><document /></el-icon>
