40bebfeddc38676dfe91277dd2ad7eac24efe56e
CMF-chenmengfei <325949568@qq.com>
2022-05-09 15:34:06 +0800

    提交冲突代码

--- components.d.ts
+++ components.d.ts
@@ -15,6 +15,7 @@ declare module '@vue/runtime-core' {
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElMenuItemGroup: typeof import('element-plus/es')['ElMenuItemGroup']
+    ElMenuMenu: typeof import('element-plus/es')['ElMenuMenu']
     ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     Footer: typeof import('./src/components/layout/Footer.vue')['default']
