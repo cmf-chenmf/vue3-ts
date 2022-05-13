aa45efce6dfd9f8910e373b2c6292874276c55aa
CMF-chenmengfei <325949568@qq.com>
2022-05-13 15:06:05 +0800

    add file

---
+++ src/components/layouts/BasicLayout.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="basic-layout">
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content bg-purple-dark"/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="12">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="8">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="8">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+      <el-col :span="8">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.el-row {
+  margin-bottom: 20px;
+}
+
+.el-row:last-child {
+  margin-bottom: 0;
+}
+
+.el-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+
+.bg-purple-dark {
+  background-color: #99a9bf;
+}
+
+.bg-purple-light {
+  background-color: #e5e9f2;
+}
+
+.bg-purple {
+  background-color: #9b59b6;
+}
+</style>
---
+++ src/components/layouts/OffsetLayout.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="6" :offset="6">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="6" :offset="6">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="6" :offset="6">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="12" :offset="6">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.el-row {
+  margin-bottom: 20px;
+}
+
+.el-row:last-child {
+  margin-bottom: 0;
+}
+
+.el-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+
+.bg-purple {
+  background-color: #9b59b6;
+}
+</style>
