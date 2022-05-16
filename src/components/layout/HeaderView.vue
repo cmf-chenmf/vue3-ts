e0dea8282299c942b2b9bd37966480b8a3ee9620
CMF-chenmengfei <325949568@qq.com>
2022-05-16 15:32:28 +0800

    重新修改布局  使用:root 新增切换主题功能

---
+++ src/components/layout/HeaderView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="layout-header">layout-header</div>
+</template>
+
+<style scoped lang="scss">
+.layout-header{
+  height: 55px;
+  border-bottom: 1px solid #dcdfe6;
+}
+</style>
---
+++ src/views/LayoutView.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import {ref, onMounted} from "vue";
+
+const html = ref<any>(null);
+onMounted(() => {
+  html.value = document.getElementsByTagName('html')[0];
+})
+/**
+ * 简单修改主题
+ */
+const toggleTheme = () => {
+  const bool = html.value.classList.contains("dark");
+  !bool ? html.value.classList.add("dark") : html.value.classList.remove("dark");
+}
+</script>
+
+<template>
+  <div class="layout-view">
+    <header class="ly-header" @click="toggleTheme">Header</header>
+    <aside class="ly-aside">Aside</aside>
+    <main class="ly-main">Main</main>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.layout-view {
+  /* header */
+  .ly-header {
+    height: var(--header-height);
+    border-bottom: 1px solid #dcdfe6;
+    position: fixed;
+    width: 100%;
+  }
+
+  /* aside */
+  .ly-aside {
+    position: fixed;
+    top: var(--header-height);;
+    left: 0;
+    width: 260px;
+
+  }
+
+  /* main */
+  .ly-main {
+    padding-top: var(--header-height);
+    padding-left: 260px;
+  }
+}
+</style>
